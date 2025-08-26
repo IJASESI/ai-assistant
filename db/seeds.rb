d609d22078e64adc7b64744b49a4699b237c5d23
Laurie Knott <knott.laurie@hotmail.be>
2025-08-26 16:21:59 +0200

    DB seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# 1. Clean the database 🗑️
+puts "Cleaning database..."
+User.destroy_all
+Car.destroy_all
+
+# 2. Create the users instances 🏗️
+joao   = User.create!(first_name: "João", last_name: "Cork Oak", email: "corkoak@gmail.com", password: "winecork")
+ricardo = User.create!(first_name: "Ricardo", last_name: "Bushes", email: "bushes@gmail.com", password: "winebushes")
+laurie = User.create!(first_name: "Laurie", last_name: "Small Rock", email: "smallrock@gmail.com", password: "wineglass")
+
+# 3. Display a message 🎉
+puts "Finished! Created #{User.count} users."
+
+# 4. Create the cars instances 🏗️
+Car.create!(brand: "Opel", model: "Astra", year: 2000, user: joao)
+Car.create!(brand: "Lamborghini", model: "Urus", year: 2025, user: ricardo)
+Car.create!(brand: "Mitsubishi", model: "Colt", year: 2009, user: laurie)
+
+# 5. Display a message 🎉
+puts "Finished! Created #{Car.count} cars."
