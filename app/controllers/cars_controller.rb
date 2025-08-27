2325cc8a1f116dce2365f389e289a58b963f9d9a
ricardo moitas <ricardo.moitas04@gmail.com>
2025-08-27 14:32:56 +0100

    select-car updated

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,18 @@
 class CarsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:brand, :model, :year)
+  end
 end
